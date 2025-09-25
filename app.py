24dbfb8728f3e575201e00760f066b744ddd4e35
Zaldiharun <zaldiharun27@gmail.com>
2025-09-25 12:47:02 +0700

    Judul Peubahan

---
+++ app.py
@@ -0,0 +1,8 @@
+def luas_segitiga():
+    a = int(input("Masukkan alas segitiga: "))
+    t = int(input("Masukkan tinggi segitiga: "))
+    luas = a * t / 2
+    print("Luas segiiga adalah:",luas)
+
+
+luas_segitiga()
